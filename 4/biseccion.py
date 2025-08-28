e1aa0e42ae96ffa1c415f88e72bc22629aa4d080
Esteban <esteban25agosto@gmail.com>
2025-08-28 17:25:32 -0600

    Add bisection method implementation in Python
    Introduces 4/biseccion.py with a function to find roots of a function using the bisection method. Also updates .gitignore to include .vscode/settings.json.

---
+++ 4/biseccion.py
@@ -0,0 +1,66 @@
+
+
+from sympy import symbols, lambdify, sympify
+import time
+f='x*exp(-x)-5-(cos(x))/(x)'
+x = symbols('x')
+
+def metodo_biseccion(f, a, b, tol=1e-8, max_iter=10000):
+    """
+    Obtiene la raiz de la función f en un rango [a, b]
+    por medio del metodo de la bisección
+
+    Parametros:
+        f: función
+        a: limite izquierdo del rango
+        b: limite derecho del rango
+        tol: error máximo permitido
+        max_iter: máximo de iteraciones permitidas
+
+    Retorna:
+        Una tupla con los siguientes valores:
+        (
+            xk: aproximación final,
+            fxk: error de la función,
+            k: cantidad de iteraciones,
+            time: tiempo de ejecución
+        )  
+    """
+
+    # Obtener el tiempo de ejecución
+    time_st = time.time()
+
+    # Funciones numericas a utilizar
+    f_sym = sympify(f, evaluate=False)
+    f_num = lambdify(x, f_sym, 'numpy')     # Función donde se aproxima el cero.
+    f_prom = lambda a, b: (a + b) / 2       # Función que obtiene el punto medio entre a y b.
+
+    # Valores iniciales
+    xk, fxk = 0.0, 0.0
+    k = 0
+
+    # Iterar la aproximación
+    for k in range(max_iter):
+        # Calcular el punto medio y el valor en la funcion f(x)
+        xk = f_prom(a, b)
+        fxk = f_num(xk)
+
+        # Verificar si se llegó al error
+        if (abs(fxk) < tol):
+            break
+
+        # Verificar teorema de bolzano
+        fa = f_num(a)
+        if ((fa * fxk) < 0):
+            b = xk
+        else:
+            a = xk
+
+    # Calcular el tiempo de ejecución
+    time_end = time.time() - time_st
+
+    # Retornar valores
+    return (xk, abs(fxk), k + 1, time_end)
+
+print(metodo_biseccion(f, -0.3, -0.1))
+
