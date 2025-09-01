dcda26a0e204339588a42044cbc3e2193a2a6687
Esteban <esteban25agosto@gmail.com>
2025-08-31 21:24:58 -0600

    Comentarios y cambios en el ejercicio 4

--- 4/methods4/secante.py
+++ 4/methods4/secante.py
@@ -7,9 +7,24 @@ x = symbols('x')
 
 def secante(f,x0, x1, tol=1e-8, max_iter=10000):
     """ 
+    Función para calcular la raíz de una función con el método
+    de la secante
+
+    Parametros:
+        f: función
+        x0, x1: valores iniciales
+        tol: error máximo permitido
+        max_iter: máximo de iteraciones permitidas
+
+    Retorna:
+        Una tupla con los siguientes valores:
+        (
+            xk: aproximación final,
+            fxk: error de la función,
+            k: cantidad de iteraciones
+        )    
     
-    
-    |"""
+    """
 
     f_sym = sympify(f, evaluate=False)
     f_num = lambdify(x, f_sym, 'numpy')
@@ -19,9 +34,6 @@ def secante(f,x0, x1, tol=1e-8, max_iter=10000):
         i = "NA"
         print("La función no puede evaluarse en los puntos iniciales dados.")
         return x2, error, i
-        
-        
-    
 
     for i in range(max_iter):
         x2 = x1 - f_num(x1)*(x1-x0)/(f_num(x1)-f_num(x0))
